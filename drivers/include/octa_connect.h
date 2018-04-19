0fba77c89c6419d48b1979815e3213fb474d83e2
iliessens <imre.liessens@telenet.be>
2018-04-19 08:56:36 +0200

    LED control v1

--- drivers/include/octa_connect.h
+++ drivers/include/octa_connect.h
@@ -2,4 +2,9 @@
 #ifndef OCTA_CONNECT_H
 #define OCTA_CONNECT_H
 
+typedef enum { RED, GREEN, BLUE} octa_led_color_t;
+
+int octa_connect_init(void);
+void octa_write_led(octa_led_color_t color, int state);
+
 #endif
--- drivers/octa_connect/octa_connect.c
+++ drivers/octa_connect/octa_connect.c
@@ -1,3 +1,50 @@
 
 #include "octa_connect.h"
+#include "periph/gpio.h"
 
+typedef struct {
+	gpio_t red;
+	gpio_t green;
+	gpio_t blue;
+} octa_leds_t;
+
+octa_leds_t octa_leds;
+
+void init_leds(void) {
+	octa_leds.green = GPIO_PIN(PORT_B,0);
+	gpio_init(octa_leds.green,GPIO_OUT);
+	gpio_write(octa_leds.green, 1); // default to off
+	
+	octa_leds.red = GPIO_PIN(PORT_D,14);
+	gpio_init(octa_leds.red,GPIO_OUT);
+	gpio_write(octa_leds.red, 1); // default to off
+	
+	octa_leds.blue = GPIO_PIN(PORT_D,15);
+	gpio_init(octa_leds.blue,GPIO_OUT);
+	gpio_write(octa_leds.blue, 1); // default to off
+}
+
+int octa_connect_init(void) {
+	
+	init_leds();
+	
+	return 0;
+}
+
+void octa_write_led(octa_led_color_t color, int state) {
+	gpio_t* pin = NULL;
+	
+	switch(color) {
+		case(RED): pin = &octa_leds.red;
+		break;
+		case(GREEN): pin = &octa_leds.green;
+		break;
+		case(BLUE): pin = &octa_leds.blue;
+		break;
+	}
+	
+	if(pin == NULL) return;
+	puts("Writing LED");
+	
+	gpio_write(*pin, (state == 1) ? 0 : 1);
+}
